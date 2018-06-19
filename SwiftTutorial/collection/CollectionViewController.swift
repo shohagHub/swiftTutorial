7aed24b76f5fa4327249516798cba30109168a8f
Saleh Enam Shohag <shohag402@gmail.com>
2018-06-19 15:29:12 +0600

    Custom cell and horizontal scrolling
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

--- SwiftTutorial/collection/CollectionViewController.swift
+++ SwiftTutorial/collection/CollectionViewController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class CollectionViewController: UIViewController {
+class CollectionViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    let cellIdentifier = "CELL"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Collection View"
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+//       
+        
 
+        registerCell()
         // Do any additional setup after loading the view.
     }
 
@@ -22,6 +30,30 @@ class CollectionViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+   
     
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath)
+//        let label = UILabel(frame: CGRect(x:100, y: 30, width: UIScreen.main.bounds.width , height: 40))
+//        label.textAlignment = .left
+//        label.lineBreakMode = .byWordWrapping
+//        label.numberOfLines = 0
+//        label.text = "cell \(indexPath.row)"
+//        cell.addSubview(label)
+        let cell: CustomCollectionViewCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellIdentifier, for: indexPath) as! CustomCollectionViewCell
+        cell.label.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    func registerCell(){
+//        let
+//        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellIdentifier)
+        collectionView.register(UINib.init(nibName: "CustomCollectionViewCell", bundle: nil), forCellWithReuseIdentifier: cellIdentifier)
+        
+    }
 
 }
--- SwiftTutorial/collection/CustomCollectionViewCell.swift
+++ SwiftTutorial/collection/CustomCollectionViewCell.swift
@@ -9,7 +9,12 @@
 import UIKit
 
 class CustomCollectionViewCell: UICollectionViewCell {
-
+    
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
