8c36c3988e0ee726744e309f3e98105a030e3151
Saleh Enam Shohag <shohag402@gmail.com>
2018-06-19 15:06:49 +0600

    Simple collection view cell without extra xib
    Signed-off-by: Saleh Enam Shohag <shohag402@gmail.com>

---
+++ SwiftTutorial/collection/CustomCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CustomCollectionViewCell.swift
+//  SwiftTutorial
+//
+//  Created by Nazia Afroz on 19/6/18.
+//  Copyright © 2018 Nazia Afroz. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
